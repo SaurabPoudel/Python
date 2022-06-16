33dc4fcc4b885d8161396fe0c4a59df4f46e0462
Saurab Poudel <69349824+SaurabPoudel@users.noreply.github.com>
2022-06-16 17:55:33 +0545

    Uploading Files

---
+++ twinkle.py
@@ -0,0 +1,9 @@
+'''This is a 
+    multiline comment
+'''
+#this is single line comment
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
